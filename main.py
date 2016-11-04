41832263d2ba5cdd7c73281bacffc642ebe2dbf1
Alexander-Andrade <veimar.94@gmail.com>
2016-11-04 16:47:09 +0300

    afgasf

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import scipy
 import scipy.ndimage
+import timeit
+from PIL import Image
+import numpy as np
+from scipy.misc import toimage
 
 if __name__=="__main__":
-    image = scipy.ndimage.imread("images.jpg")
+    print("scipy ndimage")
+    start = timeit.timeit()
+    simage = scipy.ndimage.imread("hd.jpg")
+    print(simage)
+    width, height, bytes_per_pix = simage.shape
+    image = np.reshape(simage, (width * height, bytes_per_pix))
+    end = timeit.timeit()
+    print(end-start)
+
+    # toimage(simage).show()
+
+    #print(image.data)
+
+    print("pillow")
+    start = timeit.timeit()
+    img = Image.open("hd.jpg")
+    #toimage(img).show()
+    print(np.array(img))
+    #pimgage = np.array(list(img.getdata()), np.uint8)
+    end = timeit.timeit()
+    print(end - start)
+    #print(pimgage)
+
+
